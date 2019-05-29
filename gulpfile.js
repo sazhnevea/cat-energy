749ca8c843b1de0970050ead8ea03e5502503cc1
sazhnevea <sazhnevea@gmail.com>
2019-05-29 23:54:46 +0400

    gulpfile less

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,32 @@
-"use strict";
+// "use strict";
 
 var gulp = require("gulp");
 var less = require("gulp-less");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+// var plumber = require("gulp-plumber");
+// var postcss = require("gulp-postcss");
+// var autoprefixer = require("autoprefixer");
+// var server = require("browser-sync").create();
 
-gulp.task("style", function () {
+gulp.task("less", function () {
   gulp.src("less/style.less")
-    .pipe(plumber())
+    // .pipe(plumber())
     .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-      ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    // .pipe(postcss([
+    //   autoprefixer()
+    //   ]))
+    .pipe(gulp.dest("css"));
+    // .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
-  server.init({
-    server: ".",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
+// gulp.task("serve", ["style"], function() {
+//   server.init({
+//     server: ".",
+//     notify: false,
+//     open: true,
+//     cors: true,
+//     ui: false
+//   });
 
-  gulp.watch("less/**/*.less", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
-});
+//   gulp.watch("less/**/*.less", ["style"]);
+//   gulp.watch("*.html").on("change", server.reload);
+// });
