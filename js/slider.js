e9288c32cda5f1dc1f05b491cde3e2cb0d0bc03c
Evgenii Sazhnev <sazhnevea@gmail.com>
2019-06-20 13:55:12 +0400

    верстка главной завершена

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+var fat = document.querySelector(`.slider__image--fat`);
+var slim = document.querySelector(`.slider__image--slim`);
+var slider = document.querySelector(`.slider__toggle--range`);
+
+function moveDivisor () {
+  slim.style.width = slider.value + `00` +`%`;
+  // fat.style.width = 100 - slider.value+`%`;
+
+}
+// console.log(divisor);
