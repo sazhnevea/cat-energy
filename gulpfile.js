3f7e8ac57618c337324a22b267b60e86e8d0b72b
Evgenii Sazhnev <sazhnevea@gmail.com>
2019-06-25 11:38:24 +0400

    preloader

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ var postcss = require('gulp-postcss');
 var concat = require('gulp-concat');
 var svgmin = require('gulp-svgmin');
 var cwebp = require('gulp-cwebp');
+var del = require('del');
+
 
 gulp.task('serve', function() {
     server.init({
@@ -18,6 +20,11 @@ gulp.task('serve', function() {
             baseDir: "./build"
         }
     });
+});
+
+gulp.task('html', function() {
+  return gulp.src('html/**/*.html')
+    .pipe(gulp.dest('build/'));
 });
 
 gulp.task('less', function() {
@@ -38,29 +45,35 @@ gulp.task('js', function() {
     .on('end', server.reload);
 });
 
+gulp.task('svgo', function () {
+    return gulp.src('img/**/*.svg')
+      .pipe(svgmin())
+      .pipe(gulp.dest('build/img/'));
+});
+ 
+gulp.task('cwebp', function () {
+    return gulp.src('img/**/*.{jpg,png}')
+      .pipe(cwebp())
+      .pipe(gulp.dest('build/img/'));
+});
+
+gulp.task('clean', function() {
+  return del('build');
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('fonts/*{woff,woff2}')
+      .pipe(gulp.dest('build/fonts/'))
+});
+
 gulp.task('watch', function() {
   gulp.watch(['less/**/*.less', 'less/*.less'], gulp.series('less'));
   gulp.watch('js/*.js', gulp.series('js'));
   gulp.watch('build/*.html').on('change', server.reload);
-})
+});
 
 gulp.task('default', gulp.series(
-  gulp.parallel('less', 'js'),
+  gulp.task('clean'),
+  gulp.parallel('less', 'js', 'html', 'svgo', 'cwebp', 'fonts'),
   gulp.parallel('watch', 'serve'),
   ));
-
-
-
-gulp.task('svgo', function () {
-    return gulp.src('test/1/test.svg')
-        .pipe(svgmin())
-        .pipe(gulp.dest('test/out'));
-});
-
-
- 
-gulp.task('cwebp', function () {
-    return gulp.src('test/1/test.jpg')
-    .pipe(cwebp())
-    .pipe(gulp.dest('test/out'));
-});
